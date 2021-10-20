deb9bb62c4d9c63aa17e38946950387c2dc2a3d6
Kyle Anderson <kmanderson12@gmail.com>
2021-10-20 14:09:37 -0500

    cleanup

--- pages/api/putt_logs.js
+++
@@ -1,7 +0,0 @@
-import { connectToDatabase } from '../../utils/mongodb';
-
-export default async (req, res) => {
-  const { db } = await connectToDatabase();
-  const puttLogs = await db.collection('putt_logs').find({}).toArray();
-  res.json(puttLogs);
-};
