e0a990dac977080595aed925415cf9975b526b37
Kyle Anderson <kmanderson12@gmail.com>
2021-10-19 21:39:24 -0500

    utils cleanup

--- utils/formatMoney.js
+++
@@ -1,9 +0,0 @@
-const formatMoney = money =>
-  new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  })
-    .format(money)
-    .replace(/\D00$/, '');
-
-export default formatMoney;
